a6f4b956891630d7e930ca24be44e77aea521861
Michael Moore <mcmoore32@gmail.com>
2017-12-07 11:09:48 -0500

    Right and Wrong Answer Functions working

--- app.js
+++ app.js
@@ -14,6 +14,17 @@ Object.keys(questionObject).forEach(function(key){
     '.'
   );
 })
+
+ $('input[type=radio][name=dragon]').change(function() {
+        if (this.value === questionObject.Question1[5]) {
+            console.log("Correct Answer");
+        }
+        else {
+            console.log("Wrong Answer);
+        }
+    });
+
+
 */
 
 function renderHeroPage (){
@@ -44,12 +55,35 @@ function renderQuizPage (){
   $('.js-hero').addClass('hideMe');
   $('.js-last-page').removeClass('hideMe');
   $('.js-last-page').addClass('hideMe');
+  $('.js-quiz-image').attr('src', questionObject.Question1[0]);
+  var questionHTML = "";
+  for (i=1; i<=4; i++) {
+    questionHTML = questionHTML +
+      '<input type="radio" name="dragon" value="' + questionObject.Question1[i] + '"> ' +
+      questionObject.Question1[i] + '<br>'
+  };
+  $('.js-answers').html(questionHTML);
+
+  $('input[type=radio][name=dragon]').change(function() {
+    if (this.value === questionObject.Question1[5]) {
+      $('.js-correct').removeClass('hideMe');
+      $('.js-wrong').removeClass('hideMe');
+      $('.js-wrong').addClass('hideMe');
+    } else {
+      $('.js-wrong').removeClass('hideMe');
+      $('.js-correct').removeClass('hideMe');
+      $('.js-correct').addClass('hideMe');
+    }
+    $('input[type=radio][name=dragon]').unbind();
+  });
+
+
   $('.js-quiz-button').click(function(){
     renderLastPage();
   })
 };
 
 $( document ).ready(function() {
-    renderHeroPage();
+  renderHeroPage();
 });
 
